e76f8e5cf50bba76582d2c33b4f220fe6a53535a
Abuswaleh <abuswaleh1995@gmail.com>
2023-10-08 11:42:05 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const resources = JSON.parse(
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
